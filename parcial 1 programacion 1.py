30f7654112ffd05ac6aefb8c6a00e848ccbde95f
Eloy Arana <eloyearana@gmail.com>
2026-04-09 17:07:08 +0200

    Comienzo el parcial, creo menu y parte de opcion 1

---
+++ parcial 1 programacion 1.py
@@ -0,0 +1,57 @@
+#Parcial 1 - Programación 1 
+#Sistema de Control de Inventario
+
+#declarar variables
+herramientas = []
+existencias = []
+opcion = 0
+#1)preguntar al usuario la cantidad de herramientas a cargar
+#2)Carga de Existencias: Ingresar la cantidad de unidades para cada herramienta 
+#registrada previamente, respetando el orden de ingreso
+
+while True:
+    print("Menu principal")
+    print("1. Carga Herramientas")
+    print("2. Cantidad de Existencias")
+    print("3. Visualización de Inventario")
+    print("4. Consulta Stock")
+    print("5. Reporte Agotados")
+    print("6. Alta Nuevo Producto")
+    print("7. Actialización de Stock")
+    print("8. Salir")
+    #Ingresar número del menu
+    opcion_str = input("Elija del menu: ").strip()
+    #comprobar si son numeros
+    if not opcion_str.isdigit():
+        print("Error: ingrese un número válido.")
+        continue
+
+    opcion = int(opcion_str)
+    # Validacion de opciones de menu
+    if opcion < 1 or opcion > 8:
+        print("Error: Opción fuera de rango.")
+        continue #Vuelve a mostrar el menu
+
+    if opcion == 1:
+        #Cuantas herramientas desea cargar?
+        #Pedir que ingrese productos
+        num_tools_str = input("Cantidad Herramientas: ").strip()
+        #comprobar si son numeros
+        if not num_tools_str.isdigit():
+            print("Ingrese solo números.")
+            continue
+        num_tools = int(num_tools_str)
+    
+        #Itera la cantidad de herramientas ingresadas
+        for i in range(num_tools):
+            while True:
+                tool = input("Ingrese las Herramientas: ").strip().capitalize()
+                
+                if not tool.isalpha():
+                    print("Nombre Incorrecto")
+                    continue
+                if tool in herramientas:
+                    print("Herramienta repetida")
+                    continue
+                break
+            herramientas.append(tool)
