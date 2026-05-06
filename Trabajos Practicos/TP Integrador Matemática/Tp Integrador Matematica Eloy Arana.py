157d6752c8cd1907e97852ba13702869f3185c31
Eloy Arana <eloyearana@gmail.com>
2026-05-06 21:12:45 +0200

    Agregué menu con opciones

--- Trabajos Practicos/TP Integrador Matemática/Tp Integrador Matematica Eloy Arana.py
+++ Trabajos Practicos/TP Integrador Matemática/Tp Integrador Matematica Eloy Arana.py
@@ -1,13 +1,46 @@
 ###### TRABAJO PRÁCTICO INTEGRADOR MATEMÁTICA######
 
+while True:
 
-#resultado = int((a and b) or not c)
+  print("Ingresa el número segun la operacíon deseada")
+  print("1: Para A AND B")
+  print("2: Para A OR B")
+  print("3: Para (A AND B) OR NOT C")
+  print("4: Para Salir")
 
-for a in [0,1]:
-  for b in [0,1]:
-    for c in [0,1]:
-      resultado = int((a and b) or not c)
-      print (f" {a} {b} {c} | {resultado}")
+  opcion_str = input("Elija del menu: ").strip()
+    #comprobar si son numeros
+  if not opcion_str.isdigit():
+        print("Error: ingrese un número válido.")
+        continue
+
+  opcion = int(opcion_str)
+    # Validacion de opciones de menu
+  if opcion < 1 or opcion > 4:
+      print("Error: Opción fuera de rango.")
+      continue #Vuelve a mostrar el menu
+  
+  if opcion == 4:
+     print("***Fin del programa***")
+     break
+
+  print("A B C | R")
+  print("----------")
+  for a in [0,1]:
+    for b in [0,1]:
+      for c in [0,1]:
+        
+
+        if opcion == 1:
+          resultado = int(a and b)
+
+        elif opcion == 2:
+          resultado = int(a or b)
+
+        elif opcion == 3:
+          resultado = int((a and b) or not c)
+
+        print (f"{a} {b} {c} {resultado}")
 
 
-#print(a, b, c, "|", resultado)
+
