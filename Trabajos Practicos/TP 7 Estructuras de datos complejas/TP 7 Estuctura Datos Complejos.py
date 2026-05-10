d3a8a914898529500af911c141c302355444133b
Eloy Arana <eloyearana@gmail.com>
2026-05-10 10:34:29 +0200

    Termino el 1er ejercicio, agregar elementos al dicc

---
+++ Trabajos Practicos/TP 7 Estructuras de datos complejas/TP 7 Estuctura Datos Complejos.py
@@ -0,0 +1,14 @@
+  
+#1) Dado el diccionario precios_frutas  
+#precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450}  
+#Añadir las siguientes frutas con sus respectivos precios:  
+#● Naranja = 1200  
+#● Manzana = 1500  
+#● Pera = 2300  
+#Lista ya dada con claves y valores
+precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450} 
+#update actualiza el diccionario con los elementos nuevos
+precios_frutas.update({"Naranja" : 1200, "Manzana" : 1500, "Pera" : 2300})
+print(precios_frutas)
+
+
--- Trabajos Practicos/TP Integrador Matemática/Tp Integrador Matematica Eloy Arana.py
+++ Trabajos Practicos/TP Integrador Matemática/Tp Integrador Matematica Eloy Arana.py
@@ -4,27 +4,27 @@
 
 # Bucle principal: se repite hasta que el usuario elija salir
 while True:
-  #Muestra en consola el menu principal
-  print("Ingresa el número segun la operacíon deseada")
+  #Muestra en consola el menú principal
+  print("Ingresa el número de la operación deseada")
   print("1: Para A AND B")
   print("2: Para A OR B")
   print("3: Para (A AND B) OR NOT C")
   print("4: Para Salir")
 
-  #El usuario ingresa el n del menu
-  opcion_str = input("Elija del menu: ").strip()
-  #comprueba que sean numeros, si no lo es vuelve a pedirlo
+  #El usuario ingresa el n del menú
+  opcion_str = input("Elija del menú: ").strip()
+  #comprueba que sean números, si no lo es vuelve a pedirlo
   if not opcion_str.isdigit():
-        print("Error: ingrese un número válido.")
+        print("Error: ingrese un número válido.\n")
         continue
   #Convierte el dato ingresado a entero
   opcion = int(opcion_str)
-    # Valida para aceptar solo los n del menu
+    # Valida para aceptar solo los n del menú
   if opcion < 1 or opcion > 4:
-      print("Error: Opción fuera de rango.")
-      continue #Vuelve a mostrar el menu
+      print("Error: Opción fuera de rango.\n")
+      continue #Vuelve a mostrar el menú
   
-  #Primera opción del menu
+  #Primera opción del menú
   if opcion == 1:
     #For anidado con dos valores(0, 1)
     #Crea la Tabla de verdad
@@ -32,12 +32,12 @@ while True:
     print("--------------")
     for a in [0,1]:
       for b in [0,1]:
-         #El resultado de la operacion(True, False)
+         #El resultado de la operación(True, False)
          #Se castea a int() para obtener 1 o 0
          resultado = int(a and b)
          print(f"{a}  {b} |    {resultado}")
 
-  #Segunda opción del menu
+  #Segunda opción del menú
   elif opcion == 2:
     #For anidado con dos valores(0, 1)
     #Crea la Tabla de verdad
@@ -48,7 +48,7 @@ while True:
         resultado = int(a or b)
         print(f"{a}  {b} |    {resultado}")
 
-  #Tercera opción del menu
+  #Tercera opción del menú
   elif opcion == 3:
     #For anidado con dos valores(0, 1)
     #Crea la Tabla de verdad    
@@ -60,7 +60,7 @@ while True:
       for b in [0,1]:
         for c in [0,1]:
           resultado = int((a and b) or not c)
-          print (f"{a}  {b}  {c} |    {resultado}")
+          print(f"{a}  {b}  {c} |    {resultado}")
   else:
      #Opción 4: el usuario eligió salir
      print("***Fin del programa***")
