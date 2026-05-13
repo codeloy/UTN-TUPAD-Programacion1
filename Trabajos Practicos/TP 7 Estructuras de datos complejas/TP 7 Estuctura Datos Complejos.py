7d0e67127d169b472f37c570e155a0ed64d80f6c
Eloy Arana <eloyearana@gmail.com>
2026-05-13 20:00:10 +0200

    Revisión hecha, Trabajo finalizado

--- Trabajos Practicos/TP 7 Estructuras de datos complejas/TP 7 Estuctura Datos Complejos.py
+++ Trabajos Practicos/TP 7 Estructuras de datos complejas/TP 7 Estuctura Datos Complejos.py
@@ -1,4 +1,7 @@
-  
+### TRABAJO PRACTICO 7 DATOS COMPLEJOS ###
+
+# Alumno: Eloy Arana  
+
 #1) Dado el diccionario precios_frutas  
 #precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450}  
 #Añadir las siguientes frutas con sus respectivos precios:  
@@ -9,11 +12,10 @@
 precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450} 
 #update actualiza el diccionario con los elementos nuevos
 precios_frutas.update({"Naranja" : 1200, "Manzana" : 1500, "Pera" : 2300})
-print("------Ejercicio 1------")
 print(f"{precios_frutas}\n")
 
-#2) Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código 
-#desarrollado en el punto anterior, actualizar los precios de las siguientes frutas:  
+#2) Siguiendo con el diccionario precios_frutas,
+#  actualizar los precios de las siguientes frutas:  
 #● Banana = 1330  
 #● Manzana = 1700  
 #● Melón = 2800
@@ -21,43 +23,53 @@ print(f"{precios_frutas}\n")
 precios_frutas['Banana'] = 1330
 precios_frutas['Manzana'] = 1700
 precios_frutas['Melón'] = 2800
-print("------Ejercicio 2------")
 print(f"{precios_frutas}\n")
 
 #3) Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código 
 #desarrollado en el punto anterior, crear una lista que contenga únicamente las frutas sin los 
 #precios.
+
 #Extrae las claves del dicc con keys()
 lista_frutas = precios_frutas.keys()
-lista_frutas = list(lista_frutas)
-print("------Ejercicio 3------")  
+lista_frutas = list(lista_frutas)  
 print(f"{lista_frutas}\n")
   
 #4) Escribí un programa que permita almacenar y consultar números telefónicos.  
 #• Permití al usuario cargar 5 contactos con su nombre como clave y número como valor.  
 #• Luego, pedí un nombre y mostrale el número asociado, si existe.
-"""agenda = {}
+
+#Declarar diccionario
+agenda = {}
+#Bucle para ingresar datos al diccionario
 for i in range(5):
     nombre = input("nombre: ")
     numero = input("Número de telefono: ")
+    #agenda[nombre] representa la clave y numero el valor
     agenda[nombre] = numero
 print(agenda)
-for a in agenda:
-    busqueda = input("Ingrese el nombre que busca: ")
-    if busqueda in agenda:
-        resultado = agenda.get(busqueda)
-        print(f"El número de {busqueda} es: {resultado}")
-        break
-    else:
-        print("Nombre no encontrado")
-        break"""
+
+    #Ingresa el valor buscado
+busqueda = input("Ingrese el nombre que busca: ")
+#si el valor está
+if busqueda in agenda:
+    #get() devuelve el valor de la clave ingresada
+    resultado = agenda.get(busqueda)
+    print(f"El número de {busqueda} es: {resultado}")
+#Valor no encontrado
+else:
+    print("Nombre no encontrado")
 
 #5) Solicita al usuario una frase e imprime:  
 #• Las palabras únicas (usando un set).  
-#• Un diccionario con la cantidad de veces que aparece cada palabra. 
-"""frase = input("Ingresa una frase: ")
+#• Un diccionario con la cantidad de veces que aparece cada palabra.
+
+# Usuario ingresa una frase 
+frase = input("Ingresa una frase: ")
+#Con .split() divide las palabras de la frase
 frase_cortada = frase.split(" ")
+#Las entradas se guardan en un set{}
 unicas = set(frase_cortada)
+#Declarar diccionario vacio
 recuento = {}
 for palabra  in frase_cortada:
     if palabra in recuento:
@@ -66,28 +78,32 @@ for palabra  in frase_cortada:
         recuento[palabra] = 1
 
 print(f"Palabras Unicas: {unicas}")        
-print(f"Recuendo: {recuento}")"""
+print(f"Recuendo: {recuento}")
 
 #6) Permití ingresar los nombres de 3 alumnos, 
 # y para cada uno una tupla de 3 notas. Luego, 
 #mostrá el promedio de cada alumno.  
-
-"""alumnos = {}
+#Declarar diccionario
+alumnos = {}
+#Bucle pide ingresar los 3 alumnos y 
+#las 3 notas de c/alumno
 for i in range(3):
    nombre = input(f"nombre alumno{i+1}: ")
    nota1 = int(input("Primera nota: "))
    nota2 = int(input("Segunda nota: "))
-   nota3 = int(input("Tercera nota: "))
+   nota3 = int(input("Tercera nota: ")) 
+   #Agrega los nombres y las notas en cada iteración(3)
    alumnos[nombre] = (nota1, nota2, nota3)
 
-
+#funcion que devuelve el promedio
 def promedio(notas_tupla):
     suma = sum(notas_tupla)
     return suma / 3
-
+#Con .items puedo iterar sobre la clave(nombre)
+#y el valor(notas) del diccionario
 for nombre, notas in alumnos.items():
     promedio_final = promedio(notas)
-    print(f"el promedio de {nombre} es: {promedio_final:.2f}")"""
+    print(f"el promedio de {nombre} es: {promedio_final:.2f}")
 
 
 #7) Se recibe el registro diario de asistencia a una capacitación en forma de lista. 
@@ -97,11 +113,12 @@ for nombre, notas in alumnos.items():
 #• Generá un conjunto (set) a partir de la lista y 
 # mostrar los empleados que asistieron al menos una vez (sin repetir nombres). 
 #• Indicá cuántas veces asistió cada empleado a la capacitación. 
-
+#Inicia el diccionario vacio
 contador = {}
 asistencias = ["Ana", "Luis", "Ana", "Maria", "Luis", "Pedro", "Ana"]
+#convierte la lista a set
 asistencia_unicas = set(asistencias)
-print("------Ejercicio 7------")
+print(f"Lista original: {asistencias}")
 #Recorre el set con for e imprime los valores
 print(f"Los empleados fueron: ")
 for nombre in asistencia_unicas:
@@ -119,55 +136,53 @@ for empleado, cantidad in contador.items():
     print(f"El empleado/a: {empleado} asistio:{cantidad} veces")
 print("")
 
+
 #8) Armá un diccionario donde las claves sean nombres de productos y los 
 # valores su stock. Permití al usuario: 
 # • Consultar el stock de un producto ingresado.  
 # • Agregar unidades al stock si el producto ya existe.  
 # • Agregar un nuevo producto si no existe.   
 
-"""productos = {"leche" : 3, "harina" : 2, "fideos" : 7, "lentejas" : 1, "aceite" : 5}
+productos = {"leche" : 3, "harina" : 2, "fideos" : 7, "lentejas" : 1, "aceite" : 5}
 print(f"La lista de productos es: {productos}")
 consulta = input("Ingresa el producto: ").lower()
+#Valida que el producto(clave) esté o no
 if consulta in productos:
+    #si está, pide agregar unidades(valor)
     print(f"Con {productos.get(consulta)} unidades")
     add_unidades = int(input("Unidades a agregar: "))
     productos[consulta] += add_unidades
 else:
+    #si no está, lo agrega y pide ingresar las unidades
     add_unidades = int(input(f"Unidades para {consulta}: "))
     productos[consulta] = add_unidades
-print(f"Productos totales {productos}")"""
+print(f"Productos totales {productos}")
+
 
 #9) Creá una agenda donde las claves sean tuplas de (día, hora)
-#  y los valores sean eventos. 
-#Ejemplo:  
-# agenda = {
-#           ("lunes", "10:00"): "Reunion",
-#           ("martes", "15:00"): "Clases de ingles"
-# }
 #Permití consultar qué actividad hay en cierto día y hora. 
-
-"""agenda = {}
+#Inicia dicc vacio
+agenda = {}
 cant_ingresos = int(input("número de ingresos a la agenda: "))
-
+# Cargamos el dicc de datos
 for i in range(cant_ingresos):
     dia = input("Ingresa día de la semana: ").lower()
     hora = input("Hora (HH:MM): ")
     evento = input("Ingrese el motivo: ")
-    agenda[(dia), (hora)] = evento
+    #agrega las claves como tuplas
+    agenda[(dia, hora)] = evento
 print(agenda)
+#ingresar la consulta
 consulta_d = input("Dia a consultar: ").lower()
 consulta_h = input("Hora a consultar: ")
+#Si existe lo buscado, lo imprime en pantalla
 if (consulta_d, consulta_h) in agenda:
     print(f"Actividad: {agenda[consulta_d, consulta_h]}")
 else:
-    print("No hay nada programado")"""
+    print("No hay nada programado")
 
-#10) Dado un diccionario que mapea nombres de países con sus capitales,
-#  construí un nuevo diccionario donde:  
-# • Las capitales sean las claves.  
-# • Los países sean los valores.  
-
-#Diccionario sin modificar
+# 10) Dado un diccionario Intercambiar claves por valores
+# Diccionario sin modificar
 original = {
     "Argentina" : "Buenos Aires",
     "Chile" : "Santiago",
